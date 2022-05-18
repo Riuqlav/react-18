d7d1915dcfe8e2461d368eca358517b812880f6e
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-18 09:57:05 +0200

    Use state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 const Form = () => {
+  const [username, setusername] = useState("");
   return (
     <form>
       <div>
         <label>Username</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setusername(event.target.value)}
+        />
       </div>
     </form>
   );
