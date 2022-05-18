465b9bedac234f6db886e5acf1a317b9fa6fcc3c
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-18 09:50:15 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Form from "./components/Form";
 import StyleSheet from "./components/StyleSheet";
 
 const App = () => {
   return (
     <div>
-      <StyleSheet />
+      <Form />
     </div>
   );
 };
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <div>
+        <label>Username</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
