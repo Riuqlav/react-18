b4e54548ce8409910dc7ad4bfedca581e5e748cd
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-18 08:57:08 +0200

    style sheet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import ClickHandler from "./components/ClickHandler";
-import Message from "./components/Message";
-import NameList from "./components/NameList";
-import PComp from "./components/PComp";
-import UserGreeting from "./components/UserGreeting";
+import StyleSheet from "./components/StyleSheet";
 
 const App = () => {
   return (
     <div>
-      <NameList />
+      <StyleSheet />
     </div>
   );
 };
