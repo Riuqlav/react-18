bc71abef6895f5a9eb1957c8fa66db803ecacfe3
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-18 09:21:52 +0200

    .

--- src/components/StyleSheet.js
+++ src/components/StyleSheet.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./styles.css";
 
 const StyleSheet = () => {
-  return <h1>StyleSheet</h1>;
+  return (
+    <h1 className="primary">double cheeked up on a thursday afternoon </h1>
+  );
 };
 
 export default StyleSheet;
